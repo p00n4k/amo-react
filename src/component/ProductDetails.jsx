07e5949cda4dfc7b159d5de70f16575565e7649c
p00n4k <105700844+p00n4k@users.noreply.github.com>
2024-04-06 19:58:46 +0700

    style

--- src/component/ProductDetails.jsx
+++ src/component/ProductDetails.jsx
@@ -5,30 +5,29 @@ const ProductDetails = ({ productData }) => {
     <>
       {productData && (
         <div>
-          <h2 className="section-header">Product Details</h2>
           <p className="productid">
-            <span className="productid-head">รหัสสินค้า:</span>{' '}
+            <span className="productid-head">Code:</span>{' '}
             {productData.product_stock_id}
           </p>
           <p className="productname">
-            <span className="productname-head">ข้อมูลสินค้า:</span>
+            <span className="productname-head">Info:</span>
             <span className="productname-value">
               {' '}
               {productData.product_detail}
             </span>
           </p>
           <p className="producttotal">
-            <span className="producttotal-head">มีอยู่ในสต็อก:</span>{' '}
-            {productData.product_quantity} {productData.product_unit}
+            <span className="producttotal-head">Stock:</span>{' '}
+            <span className="product_quantity">
+              {productData.product_quantity}
+            </span>{' '}
+            {productData.product_unit}
           </p>
           <p className="warehouse_id">
-            <span className="warehouse_id-head">รหัสคลัง:</span>{' '}
-            {productData.warehouse_id}
+            <span className="warehouse_id-head">คลัง:</span>{' '}
+            {productData.warehouse_id} {productData.warehouse_name}
           </p>
-          <p className="warehouse_name">
-            <span className="warehouse_name-head">ชื่อคลัง:</span>{' '}
-            {productData.warehouse_name}
-          </p>
+
           <hr />
         </div>
       )}
