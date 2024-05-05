908988aa631e4d0ef6d483bf1599e70258085bd0
p00n4k <105700844+p00n4k@users.noreply.github.com>
2024-05-05 12:55:45 +0700

    S

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import ProductDetails from './component/ProductDetails.jsx';
 
 function App() {
-  const [searchInput, setProductId] = useState('');
+  const [searchInput, setSearchInput] = useState('');
   const [productData, setProductData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSearchBYID = async (e) => {
     e.preventDefault();
+    setLoading(true); // Set loading to true while fetching data
     try {
       const response = await fetch(
         `https://damp-forest-85365-524db93b1e7f.herokuapp.com/products/search/id/${searchInput}`
@@ -16,18 +18,27 @@ function App() {
       if (!response.ok) {
         throw new Error('Failed to fetch product data');
       }
+      if (response.status === 204) {
+        setProductData([]);
+        setError('ไม่พบสินค้ารหัสนี้ในคลัง กรุณาลองใหม่อีกครั้ง');
+        setLoading(false); // Set loading to false after handling response
+        return;
+      }
       const data = await response.json();
       setProductData(data);
-      setError(null); // Clear any previous errors
+      setError(null);
     } catch (error) {
       console.error('Error fetching product data:', error);
-      setProductData(null); // Clear product data
+      setProductData(null);
       setError(error.message);
+    } finally {
+      setLoading(false); // Set loading to false after handling response
     }
   };
 
   const handleSearchBYProductName = async (e) => {
     e.preventDefault();
+    setLoading(true); // Set loading to true while fetching data
     try {
       const response = await fetch(
         `https://damp-forest-85365-524db93b1e7f.herokuapp.com/products/search/name/${searchInput}`
@@ -35,31 +46,35 @@ function App() {
       if (!response.ok) {
         throw new Error('Failed to fetch product data');
       }
+      if (response.status === 204) {
+        setProductData([]);
+        setError('ไม่พบข้อมูลสินค้านี้ในคลัง กรุณาลองใหม่อีกครั้ง');
+        setLoading(false); // Set loading to false after handling response
+        return;
+      }
       const data = await response.json();
       setProductData(data);
-      setError(null); // Clear any previous errors
+      setError(null);
     } catch (error) {
       console.error('Error fetching product data:', error);
-      setProductData(null); // Clear product data
+      setProductData(null);
       setError(error.message);
+    } finally {
+      setLoading(false); // Set loading to false after handling response
     }
   };
 
   return (
     <div>
-      <p>
-        <h1 className="title">Amo Stock Search</h1>
-      </p>
+      <h1 className="title">Amo Stock Search</h1>
       <form className="form">
-        <br />
         <input
           type="text"
           id="productId"
           value={searchInput}
           placeholder="กรุณากรอกรหัสสินค้าหรือข้อมูลสินค้า"
-          onChange={(e) => setProductId(e.target.value)}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
-        <br />
         <button type="submit" className="btn" onClick={handleSearchBYID}>
           ค้นหาด้วยรหัสสินค้า
         </button>
@@ -71,6 +86,7 @@ function App() {
           ค้นหาด้วยข้อมูลสินค้า
         </button>
       </form>
+      {loading && <div className="loading">Loading...</div>}
       {error && <p>Error: {error}</p>}
       {productData &&
         productData.map((product, index) => (
